68ac35b9fb89501d72c97ce4971f525e7205020d
Hunter L. Melton <32827054+HLMelton@users.noreply.github.com>
2018-05-10 12:14:21 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 var app = require('express')();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
-  res.send('<h1>MrUmRocks</h1>');
+  res.sendFile(__dirname + '/index.html');
 });
 
+//Logs in the console whenever a user logs on
+io.on('connection',function(socket){
+	socket.broadcast.emit('a user connected');
+	socket.on('disconnect',function(){
+		socket.broadcast.emit('a user disconnected');
+	});
+});
+
+
+//Signals that the program is running on localhost:3000
 http.listen(3000, function(){
   console.log('listening on *:3000');
 });
